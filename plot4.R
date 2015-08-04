a6a6404fbe83e6e025a18c902823d073e5f428bf
nlsum1 <ngalaisum@gmail.com>
2015-08-05 01:15:04 +0800

    minor amendment

--- plot4.R
+++ plot4.R
@@ -26,7 +26,7 @@ with(subsetData, {
   plot( subsetData$DateTime, subsetData$Sub_metering_1, type="l", xlab= "", ylab="Energy sub metering")
   lines(subsetData$DateTime,subsetData$Sub_metering_2,col="red")
   lines(subsetData$DateTime,subsetData$Sub_metering_3,col="blue")
-  legend("topright", lwd=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n", cex=0.6)  
+  legend("top", lwd=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n", cex=0.6)  
   plot(subsetData$DateTime,subsetData$Global_reactive_power,type="l", xlab="", ylab="Global_reactive_power")
   })
 
